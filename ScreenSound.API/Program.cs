e06b0ad4e03817e1824a895ea3de0b8788a657f7
JONATHAN NASCIMENTO <115124116+mamoto22@users.noreply.github.com>
2024-12-26 23:41:35 -0300

    Configuração da API e End-Point " /Artistas"

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,6 +1,28 @@
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/Artistas", () => 
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    var artista =  dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()) );
+
+    if (artista is null) 
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(artista);
+});
 
 app.Run();
