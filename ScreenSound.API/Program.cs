f3a8dd84b61759bf525ab974cbe1d631e1815665
JONATHAN NASCIMENTO <115124116+mamoto22@users.noreply.github.com>
2024-12-27 22:50:36 -0300

    Implementação do Builder.service para usar o metodoAddDbContext para refatorar os codigos do metodo MAPGET

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,21 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/Artistas", () => 
+app.MapGet("/Artistas", ([FromServices]DAL<Artista>dal) => 
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
+    
     return Results.Ok(dal.Listar());
 });
 
-app.MapGet("/Artistas/{nome}", (string nome) =>
+app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
+    
     var artista =  dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()) );
 
     if (artista is null) 
@@ -23,6 +26,14 @@ app.MapGet("/Artistas/{nome}", (string nome) =>
         return Results.NotFound();
     }
     return Results.Ok(artista);
+});
+
+app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody]Artista artista) => {
+
+    
+    dal.Adicionar(artista);
+    return Results.Ok();
+
 });
 
 app.Run();
